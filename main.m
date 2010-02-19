00708a75eb47c5aed1e6cf7ee9f13905879036c7
Abizer Nasir <abizern@abizern.org>
2010-02-19 21:24:56 +0800

    Updated the the copyright header for the main.m file.

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 //  PXSourceList
 //
 //  Created by Alex Rozanski on 16/12/2009.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//  Copyright 2009 Alex Rozanski http://perspx.com
 //
 
 #import <Cocoa/Cocoa.h>
