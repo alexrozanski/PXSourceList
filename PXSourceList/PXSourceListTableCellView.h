9811f147086ffdffb1300ed02511f0140aa7d15c
Alex Rozanski <alex@rozanski.me>
2014-01-08 20:18:40 +0000

    Document PXSourceListTableCellView.

--- PXSourceList/PXSourceListTableCellView.h
+++ PXSourceList/PXSourceListTableCellView.h
@@ -11,8 +11,26 @@
 
 @class PXSourceListBadgeView;
 
+/**
+ `PXSourceListTableCellView` is an `NSTableCellView` subclass which can be used when using `PXSourceList`
+ in view-based mode.
+ 
+ Similar to `NSTableCellView` and its `textField` and `imageView` outlets, `PXSourceListTableCellView`
+ provides a `badgeView` outlet which can be hooked up to a `PXSourceListBadgeView` in Interface Builder
+ and then configured in `sourceList:viewForItem:`.
+ */
 @interface PXSourceListTableCellView : NSTableCellView
 
+/**
+ @brief The badge view displayed by the cell.
+ @discussion When a `PXSourceListTableCellView` instance is created, a `PXSourceListTableCellView` instance
+ is *not* automatically created and set to this property (just like with `NSTableCellView` and its
+ `textField` and `imageView` properties. This property is purely declared on this class to make creating
+ table cell views for a `PXSourceList` in Interface Builder easier without having to declare your own
+ `NSTableCellView` subclass.
+ 
+ This property is typically configured in the `PXSourceListDataSource` method `sourceList:viewForItem:`.
+ */
 @property (weak, nonatomic) IBOutlet PXSourceListBadgeView *badgeView;
 
 @end
