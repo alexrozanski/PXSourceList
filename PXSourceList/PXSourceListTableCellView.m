6300fd3844f009d29ca3604a36401cf26c6cc445
Alex Rozanski <alex@rozanski.me>
2014-01-27 14:03:08 +0000

    Change positioning in PXSourceListTableCellView.
    Position the badge view in -layout automatically, as NSTableCellView does
    for the textField and imageView outlets.

--- PXSourceList/PXSourceListTableCellView.m
+++ PXSourceList/PXSourceListTableCellView.m
@@ -8,7 +8,24 @@
 //
 
 #import "PXSourceListTableCellView.h"
+#import "PXSourceListBadgeView.h"
 
 @implementation PXSourceListTableCellView
+
+- (void)layout
+{
+    [super layout];
+
+    if (!self.badgeView)
+        return;
+
+    [self.badgeView sizeToFit];
+
+    NSRect bounds = self.bounds;
+    NSSize badgeSize = self.badgeView.frame.size;
+    self.badgeView.frame = NSMakeRect(NSMaxX(bounds) - badgeSize.width,
+                                      NSMidY(bounds) - round(badgeSize.height / 2.0f),
+                                      badgeSize.width, badgeSize.height);
+}
 
 @end
