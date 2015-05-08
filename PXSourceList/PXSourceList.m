b902e04b9cf74012df6c20245936fa87c3ed96bb
Alex Rozanski <alex@rozanski.me>
2015-05-08 11:13:07 +0100

    Fix -setFlipped: deprecation warning.
    This removes old, pre-10.6 drawing code (including call to -setFlipped:) as we
    no longer support 10.6.

--- PXSourceList/PXSourceList.m
+++ PXSourceList/PXSourceList.m
@@ -425,22 +425,12 @@ NSString * const PXSLDeleteKeyPressedOnRowsNotification = @"PXSourceListDeleteKe
 											  actualIconSize.width,
 											  actualIconSize.height);
 					}
-					
-                    //Use 10.6 NSImage drawing if we can
-                    if([icon respondsToSelector:@selector(drawInRect:fromRect:operation:fraction:respectFlipped:hints:)]) {
-                        [icon drawInRect:iconRect
-                                fromRect:NSZeroRect
-                               operation:NSCompositeSourceOver
-                                fraction:1
-                          respectFlipped:YES hints:nil];
-                    }
-                    else {
-                        [icon setFlipped:[self isFlipped]];
-                        [icon drawInRect:iconRect
-                                fromRect:NSZeroRect
-                               operation:NSCompositeSourceOver
-                                fraction:1];
-                    }
+
+                    [icon drawInRect:iconRect
+                            fromRect:NSZeroRect
+                           operation:NSCompositeSourceOver
+                            fraction:1
+                      respectFlipped:YES hints:nil];
 				}
 			}
 		}
